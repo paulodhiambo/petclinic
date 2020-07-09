33fdbaa5a2ddd3349ef4d1f2cd2f5cc280bf0b75
paul_odhiambo <paulodhiambo962@gmail.com>
2020-07-09 22:13:48 +0300

    added pojos issue #2 closed

---
+++ src/main/java/com/odhiambopaul/petclinic/model/Pet.java
@@ -0,0 +1,33 @@
+package com.odhiambopaul.petclinic.model;
+
+import java.time.LocalDate;
+
+public class Pet {
+    private PetType petType;
+    private Owner owner;
+    private LocalDate localDate;
+
+    public PetType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(PetType petType) {
+        this.petType = petType;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public LocalDate getLocalDate() {
+        return localDate;
+    }
+
+    public void setLocalDate(LocalDate localDate) {
+        this.localDate = localDate;
+    }
+}
