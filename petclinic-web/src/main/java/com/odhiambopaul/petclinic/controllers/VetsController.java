0e40f365b1a63d035bc555c66f67206388ebb050
paul odhiambo <paulodhiambo962@gmail.com>
2020-09-29 20:35:16 +0300

    list vets in vets index page. closes #18

--- petclinic-web/src/main/java/com/odhiambopaul/petclinic/controllers/VetsController.java
+++ petclinic-web/src/main/java/com/odhiambopaul/petclinic/controllers/VetsController.java
@@ -1,12 +1,21 @@
 package com.odhiambopaul.petclinic.controllers;
 
+import com.odhiambopaul.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetsController {
+    private VetService vetService;
+
+    public VetsController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
