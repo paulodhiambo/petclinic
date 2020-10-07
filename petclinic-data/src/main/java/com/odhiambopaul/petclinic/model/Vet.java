4227e7cb6e7da6bdc0e82d3acb14fb565083ccf8
paul odhiambo <paulodhiambo962@gmail.com>
2020-10-07 17:56:36 +0300

    Create Vet Speciality Entity,Associate to Vet. closes #31

--- petclinic-data/src/main/java/com/odhiambopaul/petclinic/model/Vet.java
+++ petclinic-data/src/main/java/com/odhiambopaul/petclinic/model/Vet.java
@@ -1,4 +1,15 @@
 package com.odhiambopaul.petclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
+    private Set<Specialty> specialties;
+
+    public Set<Specialty> getSpecialties() {
+        return specialties;
+    }
+
+    public void setSpecialties(Set<Specialty> specialties) {
+        this.specialties = specialties;
+    }
 }
