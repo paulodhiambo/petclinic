a1e1e1149fd56eb5b7ffa8fa7b1eaed2bb7d90af
paul odhiambo <paulodhiambo962@gmail.com>
2020-10-07 19:51:27 +0300

    Specialty Map Service. closes #35

---
+++ petclinic-data/src/main/java/com/odhiambopaul/petclinic/services/SpecialtyService.java
@@ -0,0 +1,6 @@
+package com.odhiambopaul.petclinic.services;
+
+import com.odhiambopaul.petclinic.model.Specialty;
+
+public interface SpecialtyService extends CrudService<Specialty, Long> {
+}
---
+++ petclinic-data/src/main/java/com/odhiambopaul/petclinic/services/map/SpecialtyMapService.java
@@ -0,0 +1,30 @@
+package com.odhiambopaul.petclinic.services.map;
+
+import com.odhiambopaul.petclinic.model.Specialty;
+import com.odhiambopaul.petclinic.services.SpecialtyService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialtyMapService extends AbstractMapService<Specialty, Long> implements SpecialtyService {
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+}
