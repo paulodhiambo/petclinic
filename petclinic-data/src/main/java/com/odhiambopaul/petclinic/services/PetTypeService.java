94e5db31cbf058f43a1c0c5089e195c12fc287fb
paul odhiambo <paulodhiambo962@gmail.com>
2020-10-07 18:12:14 +0300

    Create PetType Map Service. closes #33

---
+++ petclinic-data/src/main/java/com/odhiambopaul/petclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.odhiambopaul.petclinic.services;
+
+import com.odhiambopaul.petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ petclinic-data/src/main/java/com/odhiambopaul/petclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,30 @@
+package com.odhiambopaul.petclinic.services.map;
+
+import com.odhiambopaul.petclinic.model.PetType;
+import com.odhiambopaul.petclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+}
